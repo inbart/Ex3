4f7e541b23b370a96a2bdd7a962d410d8c83d683
inbar <inbar.tal1@mail.huji.ac.il>
2016-03-18 14:40:15 +0200

    project number 3

---
+++ TodoListManager/app/src/main/java/com/example/inbar/todolistmanager/CustomAdapter.java
@@ -0,0 +1,80 @@
+package com.example.inbar.todolistmanager;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.text.DateFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by Inbar on 10/03/2016.
+ */
+public class CustomAdapter extends ArrayAdapter<Item> {
+
+    private final Context context;
+    private final ArrayList<Item> arrayList;
+
+    public CustomAdapter(Context context, ArrayList<Item> items) {
+        super(context, R.layout.listrow, items);
+        this.context = context;
+        this.arrayList = items;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.listrow, parent, false);
+
+        TextView titleView = (TextView) rowView.findViewById(R.id.txtTodoTitle);
+        TextView dateView = (TextView) rowView.findViewById(R.id.txtTodoDueDate);
+
+        Item item = arrayList.get(position);
+        titleView.setText(item.getTitle());
+        Date date = item.getDate();
+        if(date != null) {
+            dateView.setText(item.convertDateToString(date));
+        }
+        else{
+            dateView.setText("No due date");
+        }
+
+        Date currentDate = new Date();
+        Date noTimeCurDate = getZeroTimeDate(currentDate);
+        Date noTimeDate = getZeroTimeDate(date);
+
+        if(noTimeDate.compareTo(noTimeCurDate) < 0){
+            titleView.setTextColor(Color.RED);
+            dateView.setTextColor(Color.RED);
+        }
+
+        return rowView;
+    }
+
+    public static Date getZeroTimeDate(Date fecha) {
+        Date res = fecha;
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime( fecha );
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        res = calendar.getTime();
+
+        return res;
+    }
+
+
+}
---
+++ TodoListManager/app/src/main/java/com/example/inbar/todolistmanager/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.inbar.todolistmanager;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.view.ContextMenu;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import android.view.ContextMenu.ContextMenuInfo;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Item> arrayList = new ArrayList<Item>();
+    private CustomAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listv);
+        adapter = new CustomAdapter(this, arrayList);
+        listView.setAdapter(adapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        registerForContextMenu(listView);}
+
+        public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo){
+            if(view.getId() == R.id.listv){
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+                Item item = arrayList.get(info.position);
+                menu.setHeaderTitle(item.getTitle());
+                menu.add("Delete Item");
+                String[] arr = item.getTitle().split(" ");
+                if(arr[0].compareTo("Call") == 0){
+                    menu.add(item.getTitle());
+                }
+            }
+        }
+
+        public boolean onContextItemSelected(MenuItem item){
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+            String[] arr2 = item.getTitle().toString().split(" ");
+            if(item.getTitle() == "Delete Item"){
+                arrayList.remove(info.position);
+                adapter.notifyDataSetChanged();
+            }
+
+            else if(item.getTitle() == arrayList.get(info.position).getTitle()){
+                Intent dial = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + arr2[1]));
+                startActivity(dial);
+            }
+            return true;
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.menuItemAdd) {
+            Intent intent = new Intent(this, AddNewTodoItemActivity.class);
+            startActivityForResult(intent, 1);
+        }
+        return true;
+    }
+
+   @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       if (requestCode == 1) {
+           if (resultCode == RESULT_OK) {
+               Bundle b = data.getExtras();
+               if (b != null) {
+                   String title = (String)b.get("title");
+                   Date date = (Date)b.get("dueDate");
+                   if(!title.matches("")){
+                       Item it = new Item(title, date);
+                       arrayList.add(it);
+                       adapter.notifyDataSetChanged();
+                   }
+               }
+           }
+       }
+   }
+
+}
